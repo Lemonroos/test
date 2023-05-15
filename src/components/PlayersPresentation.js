f440adee224491ea9a6e7fdd813e083e41e95286
Lemonroos <codeassasault@gmail.com>
2023-05-15 11:27:05 +0700

    React Hook - useState(EX10)

--- src/components/PlayersPresentation.js
+++ src/components/PlayersPresentation.js
@@ -1,5 +1,7 @@
-import React from "react";
+import {useState} from "react";
+
 export default function PlayersPresentation({ players }) {
+    const [player, setPlayer] = useState([])
   //console.log(players);
   return (
     <div className="container">
@@ -10,11 +12,29 @@ export default function PlayersPresentation({ players }) {
             <h3>{player.name}</h3>
             <p className="title">{player.club}</p>
             <p>
-              <button>Detail</button>
+              <button
+                onClick={() => {
+                  setPlayer(player)
+                }}
+              >
+                <a href="#popup1" id="openPopUp">
+                  Detail
+                </a>
+              </button>
             </p>
           </div>
         </div>
       ))}
+      <div id="popup1" className="overlay">
+        <div className="popup">
+          <img src={player.img} />
+          <h2>{player.name}</h2>
+          <a className="close" href="#">
+            &times;
+          </a>
+          <div className="content">{player.info}</div>
+        </div>
+      </div>
     </div>
   );
 }
