028ac73abdc4800fe8407c6d18f52376c2c0f0bd
Lemonroos <codeassasault@gmail.com>
2023-05-15 10:16:05 +0700

    Presentational and Container Components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Navigation from "./components/Navigation";
 import Players from "./components/Players";
 import Footer from "./components/Footer";
+import Main from "./components/Main";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      <Players/>
+      {/* <Players/> BAI 9 */}
+      <Main/>
       <Footer/>
     </div>
   );
--- src/components/Players.js
+++ src/components/Players.js
@@ -78,25 +78,48 @@ export default class Player extends Component {
   }
 }*/
 
-import React, { Component } from "react"
-import { players } from "../shared/ListOfPlayers"
-export default class Players extends Component {
-  render() {
-    return (
-      <div className="container">
-        {players.map((player) => (
-          <div className="column">
-            <div className="card">
-              <img src={player.img} />
-              <h3>{player.name}</h3>
-              <p className="title">{player.club}</p>
-              <p>
-                <button>Detail</button>
-              </p>
-            </div>
+//EX7
+
+// import React, { Component } from "react"
+// import { players } from "../shared/ListOfPlayers"
+// export default class Players extends Component {
+//   render() {
+//     return (
+//       <div className="container">
+//         {players.map((player) => (
+//           <div className="column">
+//             <div className="card">
+//               <img src={player.img} />
+//               <h3>{player.name}</h3>
+//               <p className="title">{player.club}</p>
+//               <p>
+//                 <button>Detail</button>
+//               </p>
+//             </div>
+//           </div>
+//         ))}
+//       </div>
+//     )
+//   }
+// }
+
+import React from "react";
+import { players } from "../shared/ListOfPlayers";
+export default function Players() {
+  return (
+    <div className="container">
+      {players.map((player) => (
+        <div className="column">
+          <div className="card">
+            <img src={player.img} />
+            <h3>{player.name}</h3>
+            <p className="title">{player.club}</p>
+            <p>
+              <button>Detail</button>
+            </p>
           </div>
-        ))}
-      </div>
-    )
-  }
+        </div>
+      ))}
+    </div>
+  );
 }
